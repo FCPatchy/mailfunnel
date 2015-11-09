baa289a521eb466edcd74cb218272453a03b9e1f
kbortnik <kbortnik@users.noreply.github.com>
2015-11-09 22:17:25 +1000

    Modified the ember post build hook to check for the 'dist' folder existence

--- web/lib/post-build-hook/index.js
+++ web/lib/post-build-hook/index.js
@@ -2,8 +2,14 @@ module.exports = {
   name: 'post-build-hook',
 
   postBuild: function (results) {
+  	var dir = 'dist';
     var fs = this.project.require('ember-cli/node_modules/fs-extra');
-    fs.copySync('dist', '../Mailfunnel/bin/Debug/app', {
+
+    if (!fs.existsSync(dir)) {
+    	fs.mkdirSync(dir)
+    }
+    
+    fs.copySync(dir, '../Mailfunnel/bin/Debug/app', {
     	dereference: true,
     	clobber: true
     }, function(err){ console.log(err); });
